508e45cfabade6bc852f15e43af1c09e50515078
Treg <Treg>
2023-03-05 13:00:40 +0100

    kattis

---
+++ Kattis/aliennumbers.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <string>
+
+int toDec(std::string number, std::string base){
+    int ret = 0;
+    auto n_base = base.length();
+    auto n_num = number.length();
+    int power = 1;
+    for (size_t i = 0; i < n_num; i++)
+    {
+        auto c = number[n_num - i - 1];
+        auto const index = base.find(c);
+        ret += index * power;
+        power = power * n_base;
+    }
+    return ret;
+}
+
+std::string fromDec(int number, std::string base) {
+    std::string ret;
+    auto n_base = base.length();
+
+    while (number > 0) {
+        int b = number % n_base;
+        auto c = base[b];
+        ret.insert(ret.begin(), c);
+        number = number / n_base;
+    }
+
+    return ret;
+}
+
+int main()
+{
+    int T;
+    std::cin >> T;
+    for (int t = 0; t < T; t++)
+    {
+        std::string number;
+        std::cin >> number;
+        std::string source;
+        std::cin >> source;
+        std::string target;
+        std::cin >> target;
+
+        auto n = toDec(number, source);
+        auto out = fromDec(n, target);
+        std::cout << "Case #" << t + 1 << ": " << out << std::endl;
+    }
+    
+}
+
+
+int test()
+{
+    std::string base = "oF8";
+    std::cout << fromDec(toDec("F", base), base) << std::endl;
+    std::cout << fromDec(toDec("8", base), base) << std::endl;
+    std::cout << fromDec(toDec("Fo", base), base) << std::endl;
+    std::cout << fromDec(toDec("FF", base), base) << std::endl;
+    std::cout << fromDec(toDec("F8", base), base) << std::endl;
+    std::cout << fromDec(toDec("8o", base), base) << std::endl;
+    std::cout << fromDec(toDec("8F", base), base) << std::endl;
+    std::cout << fromDec(toDec("88", base), base) << std::endl;
+    std::cout << fromDec(toDec("Foo", base), base) << std::endl;
+    std::cout << fromDec(toDec("FoF", base), base) << std::endl;
+}
---
+++ Kattis/temperatureconfusion.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+int main(){
+    std::string line;
+    while (std::getline(std::cin, line)){
+        auto slash = line.find('/');
+        auto a  = stoi(line.substr(0, slash));
+        auto b = stoi(line.substr(slash + 1));
+
+        // F = 9/5 * C + 32
+        // C = 5/9 * (F - 32)
+        // F = a / b
+        // -> C = 5/9 * (a/b - 32b/b) = 5/9*((a-32b)/b) = 5a-5*32b / 9b
+        auto up = 5 * a - 5 * 32 * b;
+        auto down = 9 * b;
+
+        auto gcd = std::__gcd(up, down);
+        if (gcd < 0) // Kattis wants all outputs to have a positive denominator, so we need a positive gcd
+            gcd = gcd * -1;
+
+        std::cout << up/gcd << "/" << down/gcd << std::endl;
+    }
+}
